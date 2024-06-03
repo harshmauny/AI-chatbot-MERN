10ea4d6ee7b7fa7d4ec1931d2fc6ed707308a22d
harsh <hrmauny@ncsu.edu>
2024-06-03 16:57:40 -0400

    gitignore file added

---
+++ backend/dist/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
+});
+//# sourceMappingURL=index.js.map
