596413b582b98fc658acbe282e3a8a95b509044f
harsh <hrmauny@ncsu.edu>
2024-06-17 13:14:08 -0400

    chat controllers and routes added

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { verifyToken } from "../utils/token.manager.js";
 import { chatCompletionValidator, validate } from "../utils/validator.js";
+import {
+  deleteChats,
+  generateChatCompletion,
+  sendChatsToUser,
+} from "../controllers/chatController.js";
 
 const chatRouter = Router();
 
-chatRouter.post("/new", validate(chatCompletionValidator), verifyToken);
+chatRouter.post(
+  "/new",
+  validate(chatCompletionValidator),
+  verifyToken,
+  generateChatCompletion,
+);
+chatRouter.get("/all-chats", verifyToken, sendChatsToUser);
+chatRouter.delete("/delete", verifyToken, deleteChats);
 
 export default chatRouter;
